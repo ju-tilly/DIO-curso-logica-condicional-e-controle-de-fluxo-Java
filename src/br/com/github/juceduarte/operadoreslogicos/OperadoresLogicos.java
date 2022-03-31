362dfab32c272340600d2d9fbb8e12e0dd46f7b3
ju-tilly <juce.duarte.f@gmail.com>
2022-03-31 19:32:06 -0300

    alterações nas respostas class OperadoresLogicos

--- src/br/com/github/juceduarte/operadoreslogicos/OperadoresLogicos.java
+++ src/br/com/github/juceduarte/operadoreslogicos/OperadoresLogicos.java
@@ -8,26 +8,25 @@ public class OperadoresLogicos {
         boolean b3 = true;
         boolean b4 = false;
 
-        System.out.println("b1 && b2 " + (b1 && b2));
-        System.out.println("b1 && b3 " + (b1 && b3));
+        System.out.println("b1 && b2 " + (b1 && b2)); //false
+        System.out.println("b1 && b3 " + (b1 && b3)); //true
 
-        System.out.println("b2 || b3 " + (b2 || b3));
-        System.out.println("b2 || b4 " + (b2 || b4));
+        System.out.println("b2 || b3 " + (b2 || b3));//true
+        System.out.println("b2 || b4 " + (b2 || b4));//false
 
-        System.out.println("b1 ^ b3 " + (b1 ^ b3));
-        System.out.println("b4 ^ b1 " + (b4 ^ b1));
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));//false
+        System.out.println("b4 ^ b1 " + (b4 ^ b1));//true
 
-        System.out.println(!b1);
-        System.out.println(!b2);
+        System.out.println(!b1);//false
+        System.out.println(!b2);//true
 
         int i1 = 10;
         int i2 = 5;
         float f1 = 20f;
         float f2 = 50f;
 
-        System.out.println("((i1 + i2) < (f2 - f1)) && true " + (((i1 + i2) < (f2 - f1)) && true));
-        System.out.println("(i1 > i2) || (f2 < f1) " + ((i1 > i2) || (f2 < f1)));
-        
+        System.out.println("((i1 + i2) < (f2 - f1)) && true " + (((i1 + i2) < (f2 - f1)) && true));//true
+        System.out.println("(i1 > i2) || (f2 < f1) " + ((i1 > i2) || (f2 < f1)));//true
 
         //cálculo se recebe auxilio
         double salarioMensal = 11893.58d;
@@ -35,13 +34,13 @@ public class OperadoresLogicos {
         
         int quantidadeDependentes = 4;
         int mediaDependentes = 2;
-        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));//false
 
         boolean salarioBaixo = salarioMensal < mediaSalario;
         boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
-        System.out.println((salarioBaixo) && (muitosDependentes));
+        System.out.println((salarioBaixo) && (muitosDependentes));//false
 
         boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
-        System.out.println("recebeAxilio " + recebeAuxilio);
+        System.out.println("recebeAxilio " + recebeAuxilio); //false
     }
 }
